44fea418e4cd3a331a8a0916c0729464597dad85
Yuri Murashko <yrik.murashko@gmail.com>
2022-10-25 23:17:42 +0300

    add form to enter user name to use it as id in db

--- fp_server.py
+++ fp_server.py
@@ -1,4 +1,5 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import parse_qs
 import sqlite3
 
 hostName = "localhost"
@@ -18,14 +19,21 @@ class FingerprintServer(BaseHTTPRequestHandler):
     
     def do_POST(self):
         length = int(self.headers.get('content-length'))
-        data = self.rfile.read(length).decode('utf-8')
-        cur.execute(f"INSERT INTO fingerprints(data) VALUES('{data}');")
+        data = parse_qs(self.rfile.read(length).decode('utf-8'))
+        name = data.get('name')[0]
+        fingerprint = data.get('fingerprint')[0]
+        cur.execute(f"INSERT INTO fingerprints(id,data) VALUES('{name}','{fingerprint}');")
         conn.commit()
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes(f"Success, thank you, {name}!", "utf-8"))
         
 
 if __name__ == "__main__":      
     cur.execute("""CREATE TABLE IF NOT EXISTS fingerprints(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        id TEXT PRIMARY KEY,
         data TEXT);
         """)
     conn.commit()
